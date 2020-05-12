d01a5560913180c953808e5e1381ce78cd8572fc
[wangxianjin] <[wangxj0605@126.com]>
2020-05-12 22:41:55 +0800

    新增03-08-1

--- auto/3_常用API操作/08_页面拖拽操作.py
+++ auto/3_常用API操作/08_页面拖拽操作.py
@@ -10,5 +10,13 @@ from  time import sleep
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 driver=webdriver.Chrome()
-driver.get('http:www.baidu.com')
+driver.get('https://news.baidu.com')
 driver.maximize_window()
+ActionChains=ActionChains(driver)
+#第一种拖拽方法(精准)
+# target_elem1=driver.find_element_by_xpath('//*[@id="city_name"]/a')
+# driver.execute_script("return arguments[0].scrollIntoView();",target_elem1)
+#第二种页面拖拽方法(通过做标,不精准)
+driver.execute_script("scroll(0,5000)")
+sleep(4)
+driver.quit()
