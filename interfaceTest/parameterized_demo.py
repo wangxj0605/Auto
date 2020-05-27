98a7b10aeb966a47e2d2444d37821c223c63b6df
wangxianjin <wangxj0605@126.com>
2020-05-27 17:19:18 +0800

    05/27

--- interfaceTest/parameterized_demo.py
+++ interfaceTest/parameterized_demo.py
@@ -1,9 +1,13 @@
 import unittest
+from parameterized import parameterized
+import logging
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestAdd(unittest.TestCase):
+    @parameterized.expand([('01', 2, 2), ('02', 2, 2), ('03', 3, 3), ('04', 5, 5), ('05', 4, 4), ])
+    def test_add(self, name, a, b):
+        self.assertEqual(a, b)
+        print(name, a + b)
 
 
 if __name__ == '__main__':
